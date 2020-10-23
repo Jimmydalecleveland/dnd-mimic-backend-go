1d7a527c23ebb8d9b583221bd5120342c68b8691
Jimmy DC <jimmydalecleveland@gmail.com>
2020-10-23 15:03:53 -0600

    swap dummy data for pg requested spell data

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/friendsofgo/graphiql v0.2.2
 	github.com/graph-gophers/graphql-go v0.0.0-20201003130358-c5bdf3b1108e
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.8.0
 )
--- main.go
+++ main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/friendsofgo/graphiql"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+// This is nutty, but converting from an int32 to a string requires it to first
+// be converted to an int. This library expects a graphql.ID, which a string can
+// be converted to. This method is benchmark faster than `fmt.Sprint(i)`
+func int32ToGraphqlID(i int32) graphql.ID {
+	return graphql.ID(strconv.Itoa(int(i)))
+}
 
 type query struct{}
 
 func (_ *query) Hello() string { return "Hello, world!" }
 
 type Spell struct {
-	ID   graphql.ID
+	ID   int32
 	Name string
 }
 
@@ -24,22 +37,19 @@ type SpellResolver struct {
 	s *Spell
 }
 
-var spells = []*Spell{
-	{ID: "1", Name: "Fireball"},
-	{ID: "2", Name: "Aid"},
-}
+var spells []*Spell
 
-var spellData = make(map[graphql.ID]*Spell)
+var spellData = make(map[int32]*Spell)
 
 func (r *SpellResolver) ID() graphql.ID {
-	return r.s.ID
+	return int32ToGraphqlID(r.s.ID)
 }
 
 func (r *SpellResolver) Name() string {
 	return r.s.Name
 }
 
-func (_ *query) Spell(ctx context.Context, args struct{ ID graphql.ID }) *SpellResolver {
+func (_ *query) Spell(ctx context.Context, args struct{ ID int32 }) *SpellResolver {
 	s, ok := spellData[args.ID]
 	if ok {
 		return &SpellResolver{s}
@@ -50,14 +60,69 @@ func (_ *query) Spell(ctx context.Context, args struct{ ID graphql.ID }) *SpellR
 func (_ *query) Spells() *[]*SpellResolver {
 	// forgive me father, I know not what else to do
 	var xSpellResolver []*SpellResolver
-	for key, _ := range spellData {
+	for key := range spellData {
 		xSpellResolver = append(xSpellResolver, &SpellResolver{s: spellData[key]})
 	}
 
 	return &xSpellResolver
 }
 
+func db() {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatalf("Error loading database .env file")
+	}
+	var (
+		host     = os.Getenv("PGHOST")
+		port, _  = strconv.Atoi(os.Getenv("PGPORT"))
+		user     = os.Getenv("PGUSER")
+		dbname   = os.Getenv("PGDATABASE")
+		password = os.Getenv("PGPASSWORD")
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Connected to db")
+
+	// sqlStatement := `
+	// SELECT * FROM "Spell"
+	// `
+	// var dbSpells []Spell
+	// rows, rerr := db.Query(sqlStatement)
+	// if rerr != nil {
+	// 	panic(err)
+	// }
+
+	spellQuery := `
+		Select "ID", name FROM "Spell"
+	`
+	rows, rerr := db.Query(spellQuery)
+	if rerr != nil {
+		panic(rerr)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var singleSpell Spell
+		err := rows.Scan(&singleSpell.ID, &singleSpell.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		spells = append(spells, &singleSpell)
+	}
+}
+
 func init() {
+	db()
 	for _, s := range spells {
 		spellData[s.ID] = s
 	}
